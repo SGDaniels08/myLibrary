86cf4087d89a8847509b5c91fe4e57ea2f5b739d
Shawn Daniels <SGDaniels08@gmail.com>
2020-09-17 23:10:31 -0400

    Building out POJOs

--- src/main/java/com/docshawniels/mylibrary/Book.java
+++ src/main/java/com/docshawniels/mylibrary/Book.java
@@ -5,8 +5,23 @@ import javax.persistence.Entity;
 @Entity
 public class Book {
     // Instance Variables
+    private String title;
 
     // Constructors
+    protected Book() {}
 
-    //
+    public Book(String title) {
+        this.title = title;
+    }
+
+    // Getters and Setters
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
